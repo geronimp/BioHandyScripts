80e7bff823a92fce9a64945d32ba29545c72a8f7
Joel <joel.boyd@uqconnect.edu.au>
2015-08-10 08:31:28 +1000

    update

--- 02_working_with_GG/get_gg_taxonomy_from_accession_ids.py
+++ 02_working_with_GG/get_gg_taxonomy_from_accession_ids.py
@@ -3,20 +3,20 @@ import argparse
 parser = argparse.ArgumentParser(description='''Extraction from GG database using accession ids specified''')
 parser.add_argument('--accessions', type=str, help='Accession numbers of interes', required=True)
 parser.add_argument('--gg_taxonomy', type=str, help='GG database to extarct from', required=True)
-parser.add_argument('--mode', type=str, help='Extarct negative or positive hits', choices=['n', 'p'], required=True)
+parser.add_argument('-v', help='Extarct negative or positive hits', action="store_true", default=False)
 args = parser.parse_args()
 
-ids=[x.rstrip() for x in open(args.accessions, 'r')]
-set_ids = set(ids)
-seen=[]
+tax_dict={}
+
 for i in [x.rstrip() for x in open(args.gg_taxonomy, 'r')]:
   splt=i.split()
-  if args.mode == 'n':
-    if splt[0] not in set_ids:
-      print i
-  if args.mode == 'p':
-    if splt[0] in set_ids:
-      seen+=splt[0]
-      print i
+  tax_dict[splt[0]]=' '.join(splt[1:]).strip()
 
-exit(1)
+
+if args.v:
+  print "not right now"
+else:
+  for i in [x.strip() for x in open(args.accessions)]:
+    if i in tax_dict:
+      print "%s\t%s" % (i, tax_dict[i])
+
